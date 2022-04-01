8d91f42cdba62a8826c40e4e3478b177fd5f82f2
cesarocbu <cesar_8ab@hotmail.com>
2022-04-01 01:44:28 -0600

    Funcion calcularModa terminada

--- moda.js
+++ moda.js
@@ -1,4 +1,4 @@
-const lista1 = [
+/*const lista1 = [
     1,
     2,
     3,
@@ -25,8 +25,40 @@ lista1.map(
 );
 
 const lista1Array = Object.entries(lista1Count).sort(
-    function (valorAcumulado, nuevoValor){
-        valorAcumulado - nuevoValor
+    function (elementoA, elementoB){
+        return elementoA[1] - elementoB[1]
     }
 );
+
+const moda = lista1Array[lista1Array.length - 1];
+*/
+
+/*Convertido a funcion*/
+const lista = [];
+
+function calcularModa(lista){
+
+    const lista1Count = {};
+
+    lista.map(
+        function(elemento){
+            if (lista1Count[elemento]){
+                lista1Count[elemento] += 1;
+            }else{
+                lista1Count[elemento] = 1;
+            }
+        }
+    );
+    
+    const lista1Array = Object.entries(lista1Count).sort(
+        function (elementoA, elementoB){
+            return elementoA[1] - elementoB[1]
+        }
+    );
+    
+    const moda = lista1Array[lista1Array.length - 1];
+
+    return moda;
+}
+
 
